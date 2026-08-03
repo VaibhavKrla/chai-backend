8d6214bdebce7d16702c6d6b2b92a00efb00afb9
Vaibhav <vaibhavkrla@gmail.com>
2026-08-03 20:13:54 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -9,8 +9,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection FAILED !!!: ", err);
+  });
 /*
 First Approach 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+// Async handlers are generally of 2 types:
+// 1. try catch
+// 2. promises
+
+// Promises code below
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// try and catch code below
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
