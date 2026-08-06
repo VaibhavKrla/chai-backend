79135d3dcbfb8709feca9c8aec8dc18544160f87
Vaibhav <vaibhavkrla@gmail.com>
2026-08-07 02:21:57 +0530

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
-router.route("/register").post(registerUser);
+
+// how to inject middleware??
+// just write the middleware in the route before the controller/method
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
